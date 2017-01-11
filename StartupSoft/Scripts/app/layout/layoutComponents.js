e0610748a1b96895b1afc348ab46c87fe14eb613
hisha <hisha@DESKTOP-N0ANV12>
2017-01-11 19:06:10 +0200

    Adde links into navbartop

--- StartupSoft/Scripts/app/layout/layoutComponents.js
+++ StartupSoft/Scripts/app/layout/layoutComponents.js
@@ -1,8 +1,19 @@
 ﻿(function () {
     'use strict'
 
+    function navbartopController($window) {
+        var vm = this;
+
+        vm.goTo = function (to) {
+            $window.open(to);
+        }
+    }
+
+    navbartopController.$inject = ['$window'];
+
     angular.module('ss.layout')
         .component('navbartop', {
-            templateUrl: '/Scripts/app/layout/_navbarTop.html'
+            templateUrl: '/Scripts/app/layout/_navbarTop.html',
+            controller: navbartopController
         });
 })();
